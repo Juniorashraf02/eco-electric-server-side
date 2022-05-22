d9c94a2ce92f264c1e3a2cd00aafcf4cc2d3be6a
juniorashraf02 <shuvo.istiak11@gmail.com>
2022-05-23 01:12:49 +0600

    put method added for a specific tool

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const express = require('express');
 const cors = require('cors');
 const port = process.env.PORT || 5000;
@@ -14,29 +14,57 @@ require('dotenv').config();
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rsoub.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-async function run(){
-    try{
+async function run() {
+    try {
         await client.connect();
         const toolsCollection = client.db('eco-electric').collection('tools');
         const reviewsCollection = client.db('eco-electric').collection('reviews');
 
 
         // tools api
-        app.get('/tools', async (req, res)=>{
+        app.get('/tools', async (req, res) => {
             const query = {};
             const cursor = toolsCollection.find(query);
             const items = await cursor.toArray();
             res.send(items);
         });
 
+        // getting a specific item by id
+        app.get('/tools/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const tool = await toolsCollection.findOne(query);
+            res.send(tool);
+        });
+
+
+        // updating a specific item by _id
+        app.put('/tools/:id', async (req, res)=>{
+            const id = req.params.id;
+            const data = req.body;
+            const filter = {_id: ObjectId(id)};
+            const options = {upsert: true};
+            const updatedDoc = {
+                $set: {availableQuantity:data.modifiedQuantity}
+            };
+            const result = await toolsCollection.updateOne(filter, updatedDoc, options);
+            res.send(result);
+        });
+
+
+
+
 
         // review api
-        app.get('/reviews', async (req, res)=>{
-            const reviews=await reviewsCollection.find({}).toArray();
+        app.get('/reviews', async (req, res) => {
+            const reviews = await reviewsCollection.find({}).toArray();
             res.send(reviews);
-        })
+        });
 
-    }finally{}
+
+
+
+    } finally { }
 }
 run().catch(console.dir);
 
